a3e107ae6f74a15a4eb7bac110b34306d3b80f28
nedder3 <arij.dev@gmail.com>
2023-05-20 08:03:50 -0300

    Add Employees controller and annotation

---
+++ Fullstack API/Fullstack.API/Fullstack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fullstack.API.Controllers
+{
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
