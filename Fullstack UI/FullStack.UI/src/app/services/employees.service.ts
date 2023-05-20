e52e94dbda349c3001ce978f3930a45be8d9dbaf
nedder3 <arij.dev@gmail.com>
2023-05-20 09:04:56 -0300

    ADD environments folder and variable

--- Fullstack UI/FullStack.UI/src/app/services/employees.service.ts
+++ Fullstack UI/FullStack.UI/src/app/services/employees.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,9 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class EmployeesService {
 
-  constructor() { }
-  
-  getAllEmployees(){
+  constructor(private http: HttpClient) { }
 
+  getAllEmployees(){
+   // this.http.get()
   }
 }
