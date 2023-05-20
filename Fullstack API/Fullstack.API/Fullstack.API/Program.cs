dcb7aa49ee13612fadfc0876be4ededbddc74897
nedder3 <arij.dev@gmail.com>
2023-05-20 07:01:26 -0300

    ADD conection string and injects it

--- Fullstack API/Fullstack.API/Fullstack.API/Program.cs
+++ Fullstack API/Fullstack.API/Fullstack.API/Program.cs
@@ -1,3 +1,6 @@
+using Fullstack.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add the db context and injects it
+
+builder.Services.AddDbContext<FullStackDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FullStackConnectionString")));
 
 var app = builder.Build();
 
