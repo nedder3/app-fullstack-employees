6bda5ac1b69695ac0db1b571e1d1836d956f32c9
nedder3 <arij.dev@gmail.com>
2023-05-20 12:47:45 -0300

    ADD constructor and params getter

--- Fullstack UI/FullStack.UI/src/app/components/employees/edit-employee/edit-employee.component.ts
+++ Fullstack UI/FullStack.UI/src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-employee',
@@ -7,9 +8,16 @@ import { Component,OnInit } from '@angular/core';
 })
 export class EditEmployeeComponent implements OnInit{
 
-  constructor(){}
+  constructor(private rout: ActivatedRoute){}
 
   ngOnInit(): void{
+
+    this.rout.paramMap.subscribe({
+      next: (params) =>{
+        params.get()
+      }
+    })
+
 
   }
 
