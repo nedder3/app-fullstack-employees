f70087d3a80dc3297f0b6ee0f06b27ffc2f02959
nedder3 <arij.dev@gmail.com>
2023-05-20 06:25:38 -0300

    ADD data folder and DbContext

---
+++ Fullstack API/Fullstack.API/Fullstack.API/Data/FullStackDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Fullstack.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fullstack.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
