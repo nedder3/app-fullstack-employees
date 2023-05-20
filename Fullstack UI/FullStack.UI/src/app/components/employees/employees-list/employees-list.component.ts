e9c28b88f591b3ef1fb002f4168919580df85153
nedder3 <arij.dev@gmail.com>
2023-05-20 09:24:32 -0300

    ADD getAllEmployees implementation

--- Fullstack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
+++ Fullstack UI/FullStack.UI/src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/services/employees.service';
 
 @Component({
   selector: 'app-employees-list',
@@ -7,13 +8,20 @@ import { Employee } from 'src/app/models/employee.model';
   styleUrls: ['./employees-list.component.css']
 })
 export class EmployeesListComponent implements OnInit{
-employees: Employee[] = [
-  
-];
-  constructor(){}
+employees: Employee[] = [];
+
+  constructor(private employeesService: EmployeesService){}
 
   ngOnInit(): void{
-
+    this.employeesService.getAllEmployees()
+    .subscribe({
+      next: (employees)=>{
+        console.log(employees)
+      },
+      error: (response) =>{
+        console.log(response);
+      }
+    })
     
 
   }
